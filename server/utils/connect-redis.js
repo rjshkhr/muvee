7e23f35a9512115da475e323bf6b2be96fdeb2ab
rajshekhar26 <rjshekhar.official@gmail.com>
2021-10-09 16:18:20 +0530

    Fix formatting

--- server/utils/connect-redis.js
+++ server/utils/connect-redis.js
@@ -3,7 +3,7 @@ import Redis from 'ioredis'
 import config from './config.js'
 import logger from './logger.js'
 
-  logger.info('Connecting to', config.REDIS_URI)
+logger.info('Connecting to', config.REDIS_URI)
 
 const redisClient = new Redis(config.REDIS_URI)
 

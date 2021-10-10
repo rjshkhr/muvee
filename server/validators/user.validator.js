d939f0e65bb83b05d1ecf930ef572429e846a2f0
rajshekhar26 <rjshekhar.official@gmail.com>
2021-10-11 01:11:23 +0530

    Fix typo

--- server/validators/user.validator.js
+++ server/validators/user.validator.js
@@ -7,7 +7,7 @@ const email = Joi.string().email().min(5).max(200).required()
 const password = Joi.string().min(8).max(50).required()
 
 const userValidator = {
-  regiser: Joi.object({ name, email, password }),
+  register: Joi.object({ name, email, password }),
   login: Joi.object({ email, password })
 }
 
